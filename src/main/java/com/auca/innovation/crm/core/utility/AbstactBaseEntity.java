db6685021cc4e7d38e266c4987ac027c658b6a15
clb <buntulevycaleb@gmail.com>
2024-11-19 18:32:47 +0200

    springboot

--- src/main/java/com/auca/innovation/crm/core/utility/AbstactBaseEntity.java
+++ src/main/java/com/auca/innovation/crm/core/utility/AbstactBaseEntity.java
@@ -12,23 +12,23 @@ import java.util.UUID;
 @Getter
 @Setter
 
-public abstract class  AbstactBaseEntity {
+public abstract class AbstactBaseEntity {
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(
-            name="UUID",
-            strategy= "org.hibernate.id.UUIGenerator"
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
     )
     private UUID id;
 
-    @Column(name="state", nullable = false)
+    @Column(name = "state", nullable = false)
     @Enumerated(EnumType.STRING)
     private EDomainState state;
 
-    @Column(name="created_at", nullable = false)
+    @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
 
-    @Column(name="updated_at", nullable = false)
+    @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
 
 }
